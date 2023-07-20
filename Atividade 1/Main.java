33681e417014dc47bb2d7ca6feb141df32214955
Lucca Souza <92926937+Luccado@users.noreply.github.com>
2023-07-20 19:14:59 -0400

    Add files via upload

--- Atividade 1/Main.java
+++ Atividade 1/Main.java
@@ -5,7 +5,14 @@ class Main {
     p1.comprimento = 8;
     p1.profundidade = 2;
     p1.temperatura = "quente";
+
+    Piscina p2 = new Piscina();
+    p2.largura = 4;
+    p2.comprimento = 16;
+    p2.profundidade = 4;
+    p2.temperatura = "fria";
     
     System.out.println(p1.volumes(p1.largura,p1.comprimento, p1.profundidade));
+    System.out.println(p2.volumes(p2.largura,p2.comprimento, p2.profundidade));
   }
 }
